f7123feb29481bf76e985c47f9c1a9762a257abc
oliversteidel <67387391+oliversteidel@users.noreply.github.com>
2021-01-06 17:11:07 +0100

    Update main.js

--- drumkit/main.js
+++ drumkit/main.js
@@ -29,30 +29,39 @@ document.addEventListener("keydown", event => {
     animate(keyName);
     switch (keyName) {
         case 'a':
+            boom.currentTime = 0;
             boom.play();
             break;
         case 's':
+            clap.currentTime = 0;
             clap.play();
             break;
         case 'd':
+            hihat.currentTime = 0;
             hihat.play();
             break;
         case 'f':
+            kick.currentTime = 0;
             kick.play();
             break;
         case 'g':
+            openhat.currentTime = 0;
             openhat.play();
             break;
         case 'h':
+            ride.currentTime = 0;
             ride.play();
             break;
         case 'j':
+            snare.currentTime = 0;
             snare.play();
             break;
         case 'k':
+            tink.currentTime = 0;
             tink.play();
             break;
         case 'l':
+            tom.currentTime = 0;
             tom.play();
     }
     window.setTimeout(() => {
