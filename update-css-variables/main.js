fae7023322adeebc8a1df9dc659135ae6147be16
oliversteidel <67387391+oliversteidel@users.noreply.github.com>
2021-01-07 16:46:21 +0100

    Update main.js

--- update-css-variables/main.js
+++ update-css-variables/main.js
@@ -1,31 +1,16 @@
-const spacing = document.querySelector('input[name=spacing');
-const blur = document.querySelector('input[name=blur');
-const color = document.querySelector('input[name=color');
 
 const input = document.querySelectorAll('input');
 
-console.log(input);
-
 input.forEach(el => {
-    el.addEventListener('input', () => {
-        console.log(el.value);
+    el.addEventListener('input', () => {        
         updateValue(el.name, el.value);
     });
 });
 
-// spacing.addEventListener('input', () => {
-//     console.log(spacing.value);
-//     updateValue(spacing.name, spacing.value);
-
-// })
-
 function updateValue(name, value) {
     if(value.charAt(0) === '#') {
         document.documentElement.style.setProperty('--' + name, value);
-
     }else {
         document.documentElement.style.setProperty('--' + name, value + 'px');
-
-    }
-    
+    }    
 }
